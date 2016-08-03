ddcece6a6f60641e17c93736fc37a81ac4b39f57
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2016-08-03 14:09:48 +1000

    initial admin blueprint framework

--- scram_web/__init__.py
+++ scram_web/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import flask_bootstrap
+from .admin.views import admin_blueprint
 
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+# Register blueprints
+app.register_blueprint(admin_blueprint,url_prefix='/admin')
 
 # Specify local files for local networks
 flask_bootstrap.StaticCDN(static_endpoint='/static')
---
+++ scram_web/admin/views.py
@@ -0,0 +1,10 @@
+from flask import render_template,abort
+from jinja2 import TemplateNotFound
+from . import admin_blueprint
+
+@admin_blueprint.route('/<page>')
+def show_page(page):
+    try:
+        return "<h1>/admin/%s</h1>" % page # TODO: render_template
+    except TemplateNotFound:
+        abort(404)
